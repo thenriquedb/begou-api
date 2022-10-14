acd25ea0fb401367992489687e0907f4ddb9f567
thenriquedb <thenrique2012@gmail.com>
2022-10-14 15:48:25 -0300

    chore(api): chore(api): create animal personality table migration

---
+++ packages/api/src/shared/infra/http/typeorm/migrations/1665773107975-CreateAnimalPersonalityTable.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAnimalPersonalityTable1665773107975
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "animal_personality",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            length: "36",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("animal_personality");
+  }
+}
