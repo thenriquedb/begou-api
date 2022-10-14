28e52f3bac020c1c66251274a313238d0d56bc97
thenriquedb <thenrique2012@gmail.com>
2022-10-14 16:41:32 -0300

    feat(api): update animal routes

--- packages/api/src/shared/infra/http/routes/animals.routes.ts
+++ packages/api/src/shared/infra/http/routes/animals.routes.ts
@@ -1,11 +1,22 @@
 import { Router } from "express";
 
 import { CreateAnimalSizeController } from "@modules/animals/useCases/CreateAnimalSize/CreateAnimalSizeController";
+import { CreateAnimalPersonalityController } from "@modules/animals/useCases/CreateAnimalPersonality/CreateAnimalPersonalityController";
+import { ListAnimalPersonalitiesController } from "@modules/animals/useCases/ListAnimalPersonalities/ListAnimalPersonalitiesController";
+import { ListAnimalSizesController } from "@modules/animals/useCases/ListAnimalSizes/ListAnimalSizesController";
 
 const createAnimalSizeController = new CreateAnimalSizeController();
+const createAnimalPersonalityController =
+  new CreateAnimalPersonalityController();
+const listAnimalPersonalitiesController =
+  new ListAnimalPersonalitiesController();
+const listAnimalSizesController = new ListAnimalSizesController();
 
 const animalsRoutes = Router();
 
-animalsRoutes.post("/", createAnimalSizeController.handle);
+animalsRoutes.post("/size", createAnimalSizeController.handle);
+animalsRoutes.get("/size", listAnimalSizesController.handle);
+animalsRoutes.post("/personality", createAnimalPersonalityController.handle);
+animalsRoutes.get("/personality", listAnimalPersonalitiesController.handle);
 
 export { animalsRoutes };
