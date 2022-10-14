2b5c0c9005e6054fbbd247d099f09abe0f2b5ed3
thenriquedb <thenrique2012@gmail.com>
2022-10-14 15:29:12 -0300

    chore(api): create animal size table migration

---
+++ packages/api/src/shared/infra/http/typeorm/migrations/1665768322659-CreateAnimalSizeTable.ts
@@ -0,0 +1,37 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAnimalSizeTable1665768322659 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "animal_size",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            length: "36",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("animal_size");
+  }
+}
