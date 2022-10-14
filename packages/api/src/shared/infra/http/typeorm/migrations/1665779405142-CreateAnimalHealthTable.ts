7ae9878631b5bac1b62bcd94d19cb2f9a751134a
thenriquedb <thenrique2012@gmail.com>
2022-10-14 18:23:27 -0300

    chore(api): create animal health table migration

---
+++ packages/api/src/shared/infra/http/typeorm/migrations/1665779405142-CreateAnimalHealthTable.ts
@@ -0,0 +1,38 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAnimalHealthTable1665779405142
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "animal_health",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            length: "36",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "description",
+            type: "varchar",
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("animal_health");
+  }
+}
