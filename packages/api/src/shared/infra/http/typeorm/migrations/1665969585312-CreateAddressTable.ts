95f9a0d39ee8f2adfb35a33fbd8144f04599f396
thenriquedb <thenrique2012@gmail.com>
2022-10-17 16:22:17 -0300

    chore(api): create address table migration

---
+++ packages/api/src/shared/infra/http/typeorm/migrations/1665969585312-CreateAddressTable.ts
@@ -0,0 +1,65 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAddressTable1665969585312 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "address",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            length: "36",
+            isPrimary: true,
+          },
+          {
+            name: "street",
+            type: "varchar",
+          },
+          {
+            name: "neighborhood",
+            type: "varchar",
+          },
+          {
+            name: "complement",
+            type: "varchar",
+          },
+          {
+            name: "zip_code",
+            type: "varchar",
+            length: "8",
+            isNullable: true,
+          },
+          {
+            name: "uf_initials",
+            type: "varchar",
+            length: "2",
+            isNullable: true,
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FK_AddressCity",
+            referencedTableName: "city",
+            referencedColumnNames: ["zip_code"],
+            columnNames: ["zip_code"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+          {
+            name: "FK_AddressUf",
+            referencedTableName: "uf",
+            referencedColumnNames: ["initials"],
+            columnNames: ["uf_initials"],
+            onDelete: "SET NULL",
+            onUpdate: "SET NULL",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("address");
+  }
+}
