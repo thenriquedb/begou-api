58ac4be1b4a0bf113dfcfde98d6c688b61b172d7
thenriquedb <thenrique2012@gmail.com>
2022-10-18 16:46:11 -0300

    chore(api): create institution user table migration

---
+++ packages/api/src/shared/infra/http/typeorm/migrations/1666121662812-CreateInstitutionUserTable.ts
@@ -0,0 +1,65 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateInstitutionUserTable1666121662812
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "institution_user",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            length: "36",
+            isPrimary: true,
+          },
+          {
+            name: "institution_id",
+            type: "varchar",
+            length: "36",
+          },
+          {
+            name: "user_id",
+            type: "varchar",
+            length: "36",
+          },
+          {
+            name: "role_id",
+            type: "varchar",
+            length: "36",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FK_InstitutionID",
+            referencedTableName: "institution",
+            referencedColumnNames: ["id"],
+            columnNames: ["institution_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+          {
+            name: "FK_UserID",
+            referencedTableName: "user",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+          {
+            name: "FK_RoleID",
+            referencedTableName: "role",
+            referencedColumnNames: ["id"],
+            columnNames: ["role_id"],
+            onDelete: "RESTRICT",
+            onUpdate: "RESTRICT",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("institution_user");
+  }
+}
