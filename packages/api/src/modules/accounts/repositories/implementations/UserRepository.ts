f07faa723ae5de07d405cda1f750ab66a1e54cc0
thenriquedb <thenrique2012@gmail.com>
2022-10-17 21:56:59 -0300

    feat(api): create user repository

---
+++ packages/api/src/modules/accounts/repositories/implementations/UserRepository.ts
@@ -0,0 +1,39 @@
+import { Repository } from "typeorm";
+
+import { ICreateUserDTO } from "@modules/accounts/dtos/ICreateUserDTO";
+import { AppDataSource } from "@shared/infra/http/typeorm/data-source";
+
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUserRepository";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = AppDataSource.getRepository(User);
+  }
+
+  async create(data: ICreateUserDTO) {
+    const { email, name, password, roleId, phoneNumber } = data;
+
+    const user = this.repository.create({
+      email,
+      name,
+      password,
+      role_id: roleId,
+      phone_number: phoneNumber,
+    });
+
+    await this.repository.save(user);
+  }
+
+  async findByEmail(email: string) {
+    const user = await this.repository.findOneBy({ email });
+    return user;
+  }
+
+  async findById(id: string) {
+    const user = await this.repository.findOneBy({ id });
+    return user;
+  }
+}
