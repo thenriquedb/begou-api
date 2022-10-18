d22ab005c03c818293f10d2f43ceba37529c3e6d
thenriquedb <thenrique2012@gmail.com>
2022-10-18 15:56:00 -0300

    refactor(api): server

--- packages/api/src/shared/infra/http/server.ts
+++ packages/api/src/shared/infra/http/server.ts
@@ -1,17 +1,5 @@
-import express from "express";
-import "express-async-errors";
+import { App } from "./app";
 
-import "reflect-metadata";
-import { errorHandler } from "@shared/infra/http/middlewares/errorHandler";
+const app = new App();
 
-import "../../container";
-import "./typeorm/data-source";
-import { router } from "./routes";
-
-const app = express();
-
-app.use(express.json());
-app.use(router);
-app.use(errorHandler);
-
-app.listen(3333, () => console.log("Server is running!"));
+app.run();
