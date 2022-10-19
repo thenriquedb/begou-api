6dae90bab864367a376e1d82d5324ae3bd485426
thenriquedb <thenrique2012@gmail.com>
2022-10-19 19:57:08 -0300

    chore(api): create specie table migration

---
+++ packages/api/src/shared/infra/http/typeorm/migrations/1666219723733-CreateSpecieTable.ts
@@ -0,0 +1,33 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateSpecieTable1666219723733 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "specie",
+        columns: [
+          {
+            name: "id",
+            type: "varchar",
+            length: "36",
+            isPrimary: true,
+          },
+          {
+            name: "name",
+            type: "varchar",
+            isUnique: true,
+          },
+          {
+            name: "created_at",
+            type: "timestamp",
+            default: "CURRENT_TIMESTAMP()",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("specie");
+  }
+}
