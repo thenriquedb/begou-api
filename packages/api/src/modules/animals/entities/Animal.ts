8ca665a83fae66736d075603316e54dd52a422b6
thenriquedb <thenrique2012@gmail.com>
2022-10-19 22:52:01 -0300

    feat(api): create animal entity

---
+++ packages/api/src/modules/animals/entities/Animal.ts
@@ -0,0 +1,69 @@
+import {
+  Entity,
+  Column,
+  PrimaryColumn,
+  CreateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  ManyToMany,
+  JoinTable,
+} from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+
+import { AnimalSize } from "@modules/animals/entities/AnimalSize";
+import { Institution } from "@modules/institutions/entities/Institution";
+import { Specie } from "@modules/animals/entities/Specie";
+import { AnimalHealth } from "@modules/animals/entities/AnimalHealth";
+import { AnimalPersonality } from "@modules/animals/entities/AnimalPersonality";
+import { AnimalGenre } from "@modules/animals/enums/Genre";
+
+@Entity("animal")
+export class Animal {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column({
+    type: "enum",
+    enum: AnimalGenre,
+  })
+  genre: AnimalGenre;
+
+  @Column()
+  description: string;
+
+  @ManyToOne(() => AnimalSize)
+  @JoinColumn({ name: "size_id" })
+  size: AnimalSize;
+
+  @ManyToOne(() => Institution)
+  @JoinColumn({ name: "institution_id" })
+  institution: Institution;
+
+  @ManyToOne(() => Specie)
+  @JoinColumn({ name: "specie_id" })
+  specie: Specie;
+
+  @ManyToMany(() => AnimalHealth)
+  @JoinTable({
+    name: "animal_has_health",
+  })
+  healths: AnimalHealth[];
+
+  @ManyToMany(() => AnimalHealth)
+  @JoinTable({
+    name: "animal_has_personality",
+  })
+  personalities: AnimalPersonality[];
+
+  @CreateDateColumn()
+  created_at: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
