61bd98e23ff3af562f6048afcb1ab2a2bf83cbcb
thenriquedb <thenrique2012@gmail.com>
2022-10-20 20:08:32 -0300

    chore(api): add available column on animal table migration

---
+++ packages/api/src/shared/infra/http/typeorm/migrations/1666307141898-AddAvailableColumnOnAnimalTable.ts
@@ -0,0 +1,19 @@
+import { MigrationInterface, QueryRunner, TableColumn } from "typeorm";
+
+export class AddAvailableColumnOnAnimalTable1666307141898
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.addColumn(
+      "animal",
+      new TableColumn({
+        name: "available",
+        type: "boolean",
+        default: true,
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropColumn("animal", "available");
+  }
+}
