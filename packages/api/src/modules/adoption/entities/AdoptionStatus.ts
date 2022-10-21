d992a1e576fdc1e68f5cc71925f3212ba8d07387
thenriquedb <thenrique2012@gmail.com>
2022-10-21 15:37:31 -0300

    feat(api): create adoption status entity

---
+++ packages/api/src/modules/adoption/entities/AdoptionStatus.ts
@@ -0,0 +1,21 @@
+import { Entity, Column, PrimaryColumn, CreateDateColumn } from "typeorm";
+
+import { UUID } from "@shared/infra/cryptography/UUID";
+
+@Entity("adoption_status")
+export class AdoptionStatus {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  constructor() {
+    if (!this.id) {
+      this.id = UUID.generate();
+    }
+  }
+}
