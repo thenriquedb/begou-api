8b1385113e35f7ba16eab70c58ac65ad7728453b
thenriquedb <thenrique2012@gmail.com>
2022-10-24 20:38:53 -0300

    feat(api): list adoption requests by institution use case

---
+++ packages/api/src/modules/adoption/useCases/ListAdoptionRequestByInstitution/ListAdoptionRequestByInstitutionUseCase.ts
@@ -0,0 +1,30 @@
+import { inject, injectable } from "tsyringe";
+
+import { IAdoptionRequestRepository } from "@modules/adoption/repositories/IAdoptionRequestRepository";
+import { BadRequestError } from "@shared/errors/BadRequestError";
+
+interface IRequest {
+  institution_id: string;
+}
+
+@injectable()
+export class ListAdoptionRequestByInstitutionUseCase {
+  private adoptionRequestRepository: IAdoptionRequestRepository;
+
+  constructor(
+    @inject("AdoptionRequestRepository")
+    adoptionRequestRepository: IAdoptionRequestRepository
+  ) {
+    this.adoptionRequestRepository = adoptionRequestRepository;
+  }
+
+  async execute(data: IRequest) {
+    const { institution_id } = data;
+
+    const adoptionRequest = await this.adoptionRequestRepository.listBy({
+      institution_id,
+    });
+
+    return adoptionRequest;
+  }
+}
