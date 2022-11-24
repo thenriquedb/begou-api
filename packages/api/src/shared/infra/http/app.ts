54581e676d16e49685653afadf9e96a50a4a438b
thenriquedb <thenrique2012@gmail.com>
2022-11-23 22:53:42 -0300

    feat(api): use new global path

--- packages/api/src/shared/infra/http/app.ts
+++ packages/api/src/shared/infra/http/app.ts
@@ -16,7 +16,7 @@ export class App {
       const app = express();
 
       app.use(express.json());
-      app.use(router);
+      app.use("/api", router);
       app.use(errorHandler);
 
       app.listen(3333, () => console.log("Server is running!"));
