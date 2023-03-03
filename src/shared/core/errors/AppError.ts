79d8356633693d64484fe992a7794e9f4b3c4b75
thenriquedb <thenrique2012@gmail.com>
2023-03-03 10:41:54 -0300

    refactor: custom error AppError extends Error now

--- src/shared/core/errors/AppError.ts
+++ src/shared/core/errors/AppError.ts
@@ -1,10 +1,14 @@
-export class AppError {
+/**
+ * @see https://stackoverflow.com/a/68899842/9525489
+ */
+export class AppError extends Error {
   public readonly message: string;
   public readonly data: any;
 
   public readonly statusCode: number;
 
   constructor(message: string, statusCode = 400, data = {}) {
+    super(message);
     this.message = message;
     this.statusCode = statusCode;
     this.data = data;
